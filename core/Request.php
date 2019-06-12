74ff2ea41246fdf0c115e18cb59db9b5b2fe3fcd
tohru ito <itohru550@gmail.com>
2019-06-12 22:46:39 +0900

    ユーザのリクエスト情報を制御するクラス

---
+++ core/Request.php
@@ -0,0 +1,21 @@
+<?php
+class Request
+{
+  //todo リクエストがPOSTか判定
+
+  //todo リクエスト$_GETを取得して返す
+
+  //todo リクエスト$_POSTを取得して返す
+
+  //todo HOSTを返す、ない場合はサーバ名を返す
+
+  //todo sslか判定
+
+  //todo uriを取得して返す
+
+  //todo urlを取得して返す
+
+  //todo ベースURLを返す
+
+  //todo ファイルパスの取得
+}
