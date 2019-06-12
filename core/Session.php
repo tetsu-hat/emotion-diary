170e23456ffbb95ad0c40e654fbaa7414e29412d
tohru ito <itohru550@gmail.com>
2019-06-12 22:53:06 +0900

    セッション情報を管理するクラス

---
+++ core/Session.php
@@ -0,0 +1,21 @@
+<?php
+class Session
+{
+  //todo セッションが使える状況、セッション状況の判定に使うプロパティ
+
+  //todo コンストラクタ セッションスタートしてるか確認してスタート
+
+  //todo $_SESSION['hoge']に値を格納する
+
+  //todo $_SESSION['hoge']を取得。ない場合はnullを。
+
+  //todo $_SESSION['hoge']をunset
+
+  //todo $_SESSIONをclear
+
+  //todo sessionIdの再定義
+
+  //todo ログイン上状況の判定の値を定義
+
+  //todo ログイン上状況の判定の値を取得
+}
