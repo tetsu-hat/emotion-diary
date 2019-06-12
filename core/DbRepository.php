e16774e1f911a3e497d4e190c76fed0b5938e2ff
tohru ito <itohru550@gmail.com>
2019-06-12 22:52:32 +0900

    データベースへのアクセスを行うクラス。SQL実行時に頻出するような処理をこのクラスに抽象化。

---
+++ core/DbRepository.php
@@ -0,0 +1,17 @@
+<?php
+abstract class DbRepository
+{
+  //todo PDOインスタンスを格納するプロパティ
+
+  //todo コンストラクトタ 以下のメソッド実行
+
+  //todo PDOインスタンスの格納
+
+  //todo executeに係る処理とstmtを返す
+
+  //todo fetchに係る処理結果を返す
+
+  //todo fetchAllに係る処理結果を返す
+
+
+}
