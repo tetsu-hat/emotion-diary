8b3a6d928cf2bd0a6de70abfdf5622eb6919b559
tohru ito <itohru550@gmail.com>
2019-06-12 22:50:23 +0900

    データベース接続情報を管理するクラス

---
+++ core/DbManager.php
@@ -0,0 +1,17 @@
+<?php
+class DbManager
+{
+  //todo DB接続に関わるPDOインスタンス等を格納するプロパティの格納
+
+  //todo 接続情報の初期化とPDOのインスタンス化とそれの格納
+
+  //todo 現在connectionプロパティに格納されているインスタンスを返す
+
+  //todo
+
+  //todo
+
+  //todo medelsディレクトリにあるhogeRepository(引数:PDOインスタンス)インスタンスを返す
+
+  //todo DB接続の解放
+}
