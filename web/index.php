f40abb20a85e8946dc2430cee543a200ecf68ffa
tohru ito <itohru550@gmail.com>
2019-06-12 22:44:39 +0900

    フロントコントローラにあたるファイル

---
+++ web/index.php
@@ -0,0 +1,7 @@
+//todo オートロードを実装したファイルの読み込み
+
+//todo EmoDiaryApplicationの読み込み
+
+//todo EmoDiaryApplicationをインスタンス化（引数はfalse)
+
+//todo EmoDiaryApplication実行
---
+++ web/index_dev.php
@@ -0,0 +1,7 @@
+//todo オートロードを実装したファイルの読み込み
+
+//todo EmoDiaryApplicationの読み込み
+
+//todo EmoDiaryApplicationをインスタンス化(引数はtrue)
+
+//todo EmoDiaryApplication実行
