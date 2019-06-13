f188ec884d96b25220041a1b5d12b0022165c36c
tohru ito <itohru550@gmail.com>
2019-06-13 22:08:32 +0900

    Applicationクラスの子クラス。ルーティング定義やデータベース接続設定等記述。

---
+++ EmotionDiaryApplication.php
@@ -0,0 +1,11 @@
+<?php
+class EmotionDiaryApplication extends Application
+{
+  protected $login_action = array('account', 'signin');
+
+//todo 親ディレクトリのパスを取得
+
+//todo ルーティング定義
+
+//todo データベース接続
+}
