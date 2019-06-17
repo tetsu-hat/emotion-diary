619f0f96fbbea5442928fd1b01d4d0b881d5afa6
tohru ito <itohru550@gmail.com>
2019-06-17 22:02:45 +0900

    ルーティング定義配列中の動的パラメータ　 :hoge を正規表現で扱える形式に変換して返すメソッド実装。 考えきれなかったためパーフェクトPHPを参照。 まずはルーティング中のパスを / ごとに分割。 : が１文字目にあるパラメータを(?P<hoge>[^/]+)に変換。 そして分割したパラメータを再度 / でつなげ直す。 これをすべてのルーティングに対して行い、それを返す。

--- core/Router.php
+++ core/Router.php
@@ -1,11 +1,34 @@
 <?
 class Router
 {
-  //todo ルートを格納するプロパティ
-
+  //ルートを格納するプロパティ
+  protected $routes;
   //todo コンストラクタ 下記を実行
+  public function __construct() {
+
+  }
 
   //todo ルーティング定義配列中の動的パラメータ指定を正規表現で扱える形式に変換して返す
+  // ルーティング中の『/:』の箇所の正規化
+  public function normalizeRoutes($route_definitions) {
+    $routes = array();
+    foreach($route_definitions as $path => $parameters){
+      $tokens = explode('/',ltrim($path,'/'));
+      foreach($tokens as $i => $token) {
+        if (0 === strpos($token, ':')) {
+          $name = substr($token, 1);
+          $token = '(?P<'. $name .'>[^/]+)';
+        }
+        $tokens[$i] = $token;
+      }
+      $pattern = '/'. implode('/', $tokens);
+      $routes[$pattern] = $parameters;
+    }
+    return $routes;
+  }
 
   //todo パスと正規化したルーティングのマッチした値を返す
+  public function pathMatch($path) {
+
+  }
 }
