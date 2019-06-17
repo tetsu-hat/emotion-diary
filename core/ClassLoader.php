d475f586ece2cd859774a8f0efbb8c9873309fdb
tohru ito <itohru550@gmail.com>
2019-06-17 16:51:16 +0900

    オートロードの実行、オートロードの対象を決める、対象となったディレクトリにあるクラスファイルの読み込みに関する機能を書く

--- core/ClassLoader.php
+++ core/ClassLoader.php
@@ -2,4 +2,22 @@
 class ClassLoader
 {
 //todo オートロードの機能
+protected $directories;
+
+public function register() {
+    spl_autoload_register(array($this,'loadClass'));
+}
+
+public function registerDirectory($directory) {
+  $this->directories[] = $directory;
+}
+
+public function loadClass($class) {
+  foreach($this->directories as $directory) {
+    $file = $directory.'/'.$class.'.php';
+    if (is_readable($file)) {
+      require $this;
+    }
+  }
+}
 }
