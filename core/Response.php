ed94ad8d15411f0a3d359c0601bbe57695ebccd8
tohru ito <itohru550@gmail.com>
2019-06-18 23:47:31 +0900

    バッファの出力に関わるメソッド、バッファを格納するメソッド、ステータスコードを格納するメソッド、リダイレクト時に使う値を格納するメソッドを記述。

--- core/Response.php
+++ core/Response.php
@@ -1,13 +1,33 @@
 <?php
-class Pesponse
+class Response
 {
-  //todo 出力する内容を格納するプロパティやステータスコード等を格納するプロパティ
+  //出力する内容を格納するプロパティやステータスコード等を格納するプロパティ
+  protected $content;
+  protected $status_code = 200;
+  protected $status_text = 'OK';
+  protected $redirect_headers = array();
+  //ステータス番号200 OKで出力(出力する内容を)
+  public function output() {
+    header('HTTP/1.1 '.$this->status_code.' '.$this->status_text);
+    if(isset($this->redirect_headers)) {
+    foreach($this->redirect_headers as $name => $url) {
+      header($name .':'.$url);
+    }
+  }
+    echo $this->content;
+  }
+  //contentに出力内容を格納
+  public function store($internal_buffer) {
+    $this->content = $internal_buffer;
+  }
 
-  //todo ステータス番号200 OKで出力
-
-  //todo contentに出力内容を格納
-
-  //todo ステータスコードをセットする
-
-  //todo リダイレクトに係る値を配列のプロパティに格納
+  //ステータスコードをセットする
+  public function setStatusCode($code, $text) {
+$this->status_code = $code;
+$this->status_text = $text;
+  }
+  //リダイレクトに係る値を配列のプロパティに格納
+  public function setRedirectHeaders($name, $url) {
+    $redirect_headers[$name] = $url;
+  }
 }
