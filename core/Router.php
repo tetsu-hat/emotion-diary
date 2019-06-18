86e00b3d424df207c066fa5c8c0f52eb93e644e4
tohru ito <itohru550@gmail.com>
2019-06-18 16:40:27 +0900

    コンストラクタの処理を追加。
    正規化したルーティングとパスのマッチングを行い、マッチしたルートパスのパラメータの配列を返すメソッドの追加。

--- core/Router.php
+++ core/Router.php
@@ -4,11 +4,11 @@ class Router
   //ルートを格納するプロパティ
   protected $routes;
   //todo コンストラクタ 下記を実行
-  public function __construct() {
-
+  public function __construct($route_definitions) {
+    $this->routes = $this->normalizeRoutes($route_definitions)
   }
 
-  //todo ルーティング定義配列中の動的パラメータ指定を正規表現で扱える形式に変換して返す
+  // ルーティング定義配列中の動的パラメータ指定を正規表現で扱える形式に変換して返す
   // ルーティング中の『/:』の箇所の正規化
   public function normalizeRoutes($route_definitions) {
     $routes = array();
@@ -27,8 +27,19 @@ class Router
     return $routes;
   }
 
-  //todo パスと正規化したルーティングのマッチした値を返す
+  //パスと正規化したルーティングのマッチした値を返す
   public function pathMatch($path) {
-
+    //$pathの1文字目が / か確認。ない場合は付与。
+  if (substr($path, 0, 1) !=='/') {
+    $path = '/'.$path;
+  }
+    //$this->routesとマッチングを行う。
+    //マッチしたら:hoge等の箇所を名前付きで値を取得し元の配列に加えて返す。falseの時はfalseを返す。
+    foreach($routes as $route => $parameters){
+    if(preg_match('#^'.$route.'$#', $path, $matches) {
+      return array_merge($parameters,$matches);
+    }
+  }
+    return false;
   }
 }
