8e750a3269766ee4e9e86c7ffea44385df824418
tohru ito <itohru550@gmail.com>
2019-06-19 15:56:05 +0900

    セッションが使える状況か判定しsession_start()するメソッド、任意のセッションに値を格納するメソッド、任意のセッションに値を取得するメソッド、任意のセッションの値を解除するメソッド、セッションに格納されたすべての値を解除するメソッド、セッションIDを再定義するメソッド、ログイン状況を判定に使用するセッションに値を格納するメソッド、ログイン状況を判定に使用するセッションの値を取得するメソッドを記述。

--- core/Session.php
+++ core/Session.php
@@ -1,21 +1,53 @@
 <?php
 class Session
 {
-  //todo セッションが使える状況、セッション状況の判定に使うプロパティ
+  //セッションが使える状況、セッション状況の判定、ログイン状況の判定に使うプロパティ
+  protected $session_started = false;
+  protected $authenticated = false;
+  //コンストラクタ セッションスタートしてるか確認してスタート
+  public function __construct() {
+    if($session_started !== false) {
+      session_start();
+      $this->session_started = true;
+    }
+  }
+  //$_SESSION['hoge']に値を格納する
+  public function set($name, $value) {
+    $_SESSION[$name] = $value;
+  }
+  //$_SESSION['hoge']を取得。ない場合はnullを。
+  public function get($name) {
+    if(isset($_SESSION[$name])) {
+      return $_SESSION[$name];
+    }
+    return null;
+  }
 
-  //todo コンストラクタ セッションスタートしてるか確認してスタート
+  //$_SESSION['hoge']をunset
+  public function release($name) {
+    unset($_SESSION[$name]);
+  }
 
-  //todo $_SESSION['hoge']に値を格納する
+  //$_SESSIONをclear
+  public function clear() {
+    $_SESSION = array();
+  }
 
-  //todo $_SESSION['hoge']を取得。ない場合はnullを。
+  //sessionIdの再定義
+  public function redefinitionSessionId() {
+    if($this->authenticated !==true) {
+      session_regenerate_id($delete_old_session = true);
+      $this->authenticated =true;
+    }
+  }
 
-  //todo $_SESSION['hoge']をunset
-
-  //todo $_SESSIONをclear
-
-  //todo sessionIdの再定義
-
-  //todo ログイン上状況の判定の値を定義
-
-  //todo ログイン上状況の判定の値を取得
+  //ログイン状況の判定の値を定義
+  public function serAuthenticated($bool) {
+    $this->set('_authenticated', $bool) ;
+    $this->redefinitionSessionId();
+  }
+  //ログイン状況の判定の値を取得
+  public function isAuthenticated() {
+    return get('_authenticated');
+  }
 }
