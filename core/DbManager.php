2137a50d1c57e6a41217e2f8fd819078145beda8
tohru ito <itohru550@gmail.com>
2019-06-20 17:48:22 +0900

    接続情報の初期化とPDOのインスタンス化とそれの格納をするメソッド、connectionプロパティに格納されているインスタンスを返すメソッド、modelsディレクトリにあるhogeRepository(引数:PDOインスタンス)インスタンスを格納、そして返すメソッド、DB接続を解放するメソッドを記述。

--- core/DbManager.php
+++ core/DbManager.php
@@ -1,17 +1,56 @@
 <?php
 class DbManager
 {
-  //todo DB接続に関わるPDOインスタンス等を格納するプロパティの格納
-
+  //DB接続に関わるPDOインスタンス等を格納するプロパティ
+  protected $connection = '';
+  protected $repositories = array();
   //todo 接続情報の初期化とPDOのインスタンス化とそれの格納
+  public function connect($parameters) {
+    $db_parameters = array_merge(array(
+      'dsn'      =>'',
+      'user'     =>'',
+      'password' =>'',
+      'options'  => array(),
+    ),$parameters);
 
-  //todo 現在connectionプロパティに格納されているインスタンスを返す
+    $pdo = new PDO(
+      $db_parameters['dsn'],
+      $db_parameters['user'],
+      $db_parameters['password'],
+      $db_parameters['options']
+    );
 
-  //todo
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  //todo
+    $this->connection = $pdo;
+  }
+  //現在connectionプロパティに格納されているインスタンスを返す
+  public function getConnection() {
+    if(!empty($this->connection)) {
+      return $this->connection;
+    }
+    return false;
+  }
+  //modelsディレクトリにあるhogeRepository(引数:PDOインスタンス)インスタンスを格納、そして返す
+  public function getRepository($repository_name) {
+    $repository_class = ucfirst($repository_name.'Repository');
+    //hogeRepositoryクラスが存在するか
+    if (isset($repository_class)) {
+      //hogeRepositoryインスタンスがすでに存在するか
+    if (!isset($this->repositories[$repository_name])) {
+      $repository = new $repository_class($this->connection);
+      $this->repositories[$repository_name] = $repository;
+      return $repository;
+    }
+    return $this->repositories[$repository_name];
+    }
+    return false;
 
-  //todo medelsディレクトリにあるhogeRepository(引数:PDOインスタンス)インスタンスを返す
+  }
+  //DB接続の解放
+  public function release() {
+    unset($this->connection);
 
-  //todo DB接続の解放
+    unset($this->repositories);
+  }
 }
