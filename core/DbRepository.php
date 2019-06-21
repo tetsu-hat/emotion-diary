eb3910b8262f487647e4f6485a84be201cb5d849
tohru ito <itohru550@gmail.com>
2019-06-21 15:27:58 +0900

    PDOインスタンスを格納するメソッド、prepareの処理を含んだexecute()メソッド,fetchとfetchAllにprepare()とexecute()の処理を追加しパラメータを設定して今後使用する際の記述を簡単にしたfetch()メソッドとfetchAllメソッドを記述。

--- core/DbRepository.php
+++ core/DbRepository.php
@@ -1,17 +1,32 @@
 <?php
 abstract class DbRepository
 {
-  //todo PDOインスタンスを格納するプロパティ
+  //PDOインスタンスを格納するプロパティ
+  protected $connection;
+  //コンストラクトタ 以下のメソッド実行
+  public function __construct($pdo) {
+    $this->setConnection($pdo);
+  }
+  //PDOインスタンスの格納
+  public function setConnection($pdo) {
+    $this->connection = $pdo;
+  }
+  //executeに係る処理とstmtを返す
+  public function execute($sql,$parameters) {
+    $stmt = $this->connection->prepare($sql);
+    $stmt->execute($parameters);
+    return $stmt;
+  }
+  //fetchに係る処理結果を返す
+  public function fetch($sql,$parameters) {
+    $stmt = $this->execute($sql,$parameters)->fetch(PDO::FETCH_ASSOC);
+    return $stmt;
+  }
 
-  //todo コンストラクトタ 以下のメソッド実行
-
-  //todo PDOインスタンスの格納
-
-  //todo executeに係る処理とstmtを返す
-
-  //todo fetchに係る処理結果を返す
-
-  //todo fetchAllに係る処理結果を返す
-
+  //fetchAllに係る処理結果を返す
+  public function fetchAll($sql,$parameters) {
+    $stmt = $this->execute($sql,$parameters)->fetchAll(PDO::FETCH_ASSOC);
+    return $stmt;
+  }
 
 }
