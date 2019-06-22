9811874fce709d5959cee828cfd13bbddf07e7c3
tohru ito <itohru550@gmail.com>
2019-06-22 11:55:02 +0900

    デバッグを設定するメソッド、最低限の準備のため各クラスのインスタンス化とそれを格納するメソッド、各インスタンスを返すメソッド、各ディレクトリまでのパスを返すメソッドの記述と子クラスに処理を記述するgetDirectoryRoot()、registerRoutes()、configureConnection()メソッド追加。

--- core/Application.php
+++ core/Application.php
@@ -1,24 +1,101 @@
 <?php
 abstract class Application
 {
-  //todo インスタンスを格納するプロパティ
+  protected $debug = false;
+  //インスタンスを格納するプロパティ
+  protected $request;
+  protected $router;
+  protected $session;
+  protected $db_manager;
+  protected $response;
 
-  //todo コンストラクト
+  //コンストラクタ
+  public function __construct($debug)
+  {
+    $this->setDebugMode($debug);
+    $this->initialize();
+    $this->configure();
+  }
 
-  //todo デバッグの設定
+  //デバッグの設定
+  protected function setDebugMode($debug)
+  {
+    if($debug) {
+      ini_set('display_errors', "On");
+      error_reporting(-1);
+    } else {
+      ini_set('display_errors', "Off");
+    }
+  }
 
-  //todo 最低限の準備のため各クラスのインスタンス化と格納
+  //最低限の準備のため各クラスのインスタンス化と格納
+  protected function initialize()
+   {
+    $this->request = new Request();
+    $this->router = new Router($this->registerRoutes());
+    $this->session = new Sesison();
+    $this->db_manager =new DbManager();
+    $this->response = new Response();
+  }
 
-  //todo $debugの値を返す
+  abstract public function getDirectoryRoot();
 
-  //todo 各インスタンスを返す
+  abstract public function registerRoutes();
 
-  //todo 各ディレクトリまでのパスを返す
+  abstract protected function configureConnection();
+
+  //各インスタンスを返す
+  public function getRequest()
+   {
+    return $this->request;
+  }
+
+  public function getRouter()
+  {
+    return $this->router;
+  }
+
+  public function getSession()
+  {
+    return $this->session;
+  }
+
+  public function getDbManager()
+   {
+    return $this->request;
+  }
+
+  public function getResponse()
+   {
+    return $this->response;
+  }
+  //各ディレクトリまでのパスを返す
+  public function getControllersDir()
+  {
+return $this->getDirectoryRoot().'/controllers';
+  }
+
+  public function getViwesDir()
+  {
+    return $this->getDirectoryRoot().'/views';
+  }
 
   //todo リクエストを受けてから出力に至るまでの全体の流れの処理
+  public function run()
+  {
+
+  }
 
   //todo 404出力
+  public function render404($e)
+  {
+
+  }
 
   //todo コントローラの呼び出しから出力内容を返す処理まで
+  public function runActionController()
+  {
+
+  }
 
 }
