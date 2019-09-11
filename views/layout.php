f3c514931e3bf63e7f3a7e3b3e9032b532076025
tohru ito <itohru550@gmail.com>
2019-09-11 18:11:56 +0900

    見た目で共通する箇所のレイアウトファイル

--- views/layout.php
+++ views/layout.php
@@ -1,30 +1,62 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charest="utf-8">
-  <title><?php if(isset($title)){echo $this->escape($title);} ?>-'Emo-Diary'</title>
-<link rel="stylesheet" href="">
+  <meta charset="utf-8">
+  <title><?php if(isset($title)){echo $this->escape($title);} ?>-Emo-Diary</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="stylesheet"  href=" <?php echo $this->escape($stage_url);?>css/normalize.css">
+  <link rel="stylesheet" href=" <?php  echo $this->escape($stage_url);?>css/style.css">
+  <script type="text/javascript" src="<?php echo $this->escape($stage_url); ?>js/menu.js"></script>
+</head>
 <body>
-  <header>
-    <h1>EmoDiary</h1>
-    <nav>
-  <ul>
-    <li>ようこそ<?php if(isset($user)){echo $this->escape($user['user_name']);} ?>さん</li>
-    <li><a href="<?php echo $this->escape($base_url); ?>/personal">マイページ</a></li>
-    <li><a href="<?php echo $this->escape($base_url); ?>/service/emotions">感情の種類</a></li>
-    <li><a href="<?php echo $this->escape($base_url); ?>/service/index">使い方</a></li>
-    <?php if($session->isAuthenticated()): ?>
-    <li><a href="<?php echo $this->escape($base_url); ?>/account/signin">サインイン</a></li>
-  <?php else: ?>
-    <li><a href="<?php echo $this->escape($base_url); ?>/account/signout">サインアウト</a></li>
-  <?php endif; ?>
-  </ul>
+  <div id="page">
+    <header id="header">
+      <div  class="title"><h1>EmoDiary</h1>
+      <a href="<?php echo $base_url; ?>/"><img class="titleImage" src="<?php echo $this->escape($stage_url); ?>images/note2.jpeg"></a>
+    </div>
+      <nav class="globalNavi">
+        <ul>
+          <li><?php if(isset($user)):?>
+            <a href="<?php echo $this->escape($base_url); ?>/personal">
+              <?php echo $this->escape($user['name']).'さん'; ?></a></li>
+            <?php else: ?>
+              <?php echo 'ゲストさん'; ?>
+            <?php endif; ?>
+            <?php if(!$session->isAuthenticated()): ?>
+              <li><a href="<?php echo $this->escape($base_url); ?>/account/signin">サインイン</a></li>
+            <?php else: ?>
+              <li><a href="<?php echo $this->escape($base_url); ?>/account/signout">サインアウト</a></li>
+            <?php endif; ?>
+          </ul>
+        </nav>
+      </header>
 
-  <nav>
-  </header>
-  <div>
-    <?php echo $content; ?>
+      <div id="hamburgerBtn">
+      <img src="<?php echo $this->escape($stage_url); ?>images/btn1.jpg">
+      </div>
+
+      <div id="sideMenu" class="">
+        <nav>
+          <ul>
+            <li>ようこそ<?php if(isset($user)){echo $this->escape($user['name']).'さん';} ?></li>
+            <li><a href="<?php echo $this->escape($base_url); ?>/personal">マイページ</a></li>
+            <li><a href="<?php echo $this->escape($base_url); ?>/service/emotions">感情の種類</a></li>
+            <li><a href="<?php echo $this->escape($base_url); ?>/service/index">使い方</a></li>
+            <?php if(!$session->isAuthenticated()): ?>
+              <li><a href="<?php echo $this->escape($base_url); ?>/account/signin">サインイン</a></li>
+            <?php else: ?>
+              <li><a href="<?php echo $this->escape($base_url); ?>/account/signout">サインアウト</a></li>
+            <?php endif; ?>
+          </ul>
+        </nav>
+      </div>
+      <div id="surface" class=""></div>
+      <div  id="main">
+        <?php echo $content; ?>
+      </div>
+      <footer id ="footer">
+        <p id="copyright"><small>Copyright&copy; 2019 @EmoDiary All Rights Reserved.</small></p>
+      </footer>
+
   </div>
-  <footer>
-  </footer>
-</body>
+  </body>
