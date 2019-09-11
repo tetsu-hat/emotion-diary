eda90259b954708a0f5bf8fac12a3b186266a92e
tohru ito <itohru550@gmail.com>
2019-09-11 18:13:37 +0900

    フォームの入力で誤りがあるときに表示する注意文のレイアウトファイル

--- views/errors.php
+++ views/errors.php
@@ -1,5 +1,7 @@
-<div>
-  <?php for($i=0;$i++;$i<count($errors)): ?>
-  <p><?php echo $this->escape($errors[$i]); ?></p>
+<div class="errors">
+  <ul>
+  <?php for($i=0;$i<count($errors);$i++): ?>
+  <li><?php echo $this->escape($errors[$i]); ?></li>
 <?php endfor; ?>
+</ul>
 </div>
