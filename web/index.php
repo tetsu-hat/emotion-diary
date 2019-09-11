01cbf77029c7c3a49cbbb467c320063b084c0c97
tohru ito <itohru550@gmail.com>
2019-09-11 14:25:08 +0900

    メモを一部削除。

--- web/index.php
+++ web/index.php
@@ -1,7 +1,10 @@
-//todo オートロードを実装したファイルの読み込み
-
-//todo EmoDiaryApplicationの読み込み
-
-//todo EmoDiaryApplicationをインスタンス化（引数はfalse)
-
-//todo EmoDiaryApplication実行
+<?php
+//オートロードを実装したファイルの読み込み
+require '../automatic_execution.php';
+//EmoDiaryApplicationの読み込み
+require '../EmotionDiaryApplication.php';
+//EmoDiaryApplicationをインスタンス化(引数はfalse)
+$application = new EmotionDiaryApplication(false);
+//EmoDiaryApplication実行
+$application->run();
+?>
