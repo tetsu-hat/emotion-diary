31512a1024abf70f1b9241e98d1bee859dd43a2e
tohru ito <itohru550@gmail.com>
2019-09-11 18:11:15 +0900

    感情の説明一覧ページ、使い方ページ(説明未入力)の見た目に関するファイル

--- views/service/emotions.php
+++ views/service/emotions.php
@@ -1,15 +1,21 @@
+<?php $this->setTitle('title', '感情一覧') ?>
+<div id="serviceEmotions">
+  <div class="topImage">
+  <img src="<?php echo $this->escape($stage_url);?>images/bookImage5.jpg">
+  </div>
 <article>
-for3回(ポジねが他)
-<?php for($i=0;$i++;$i<3): ?>
-  <section>
-    <h2><?php if(isset($devides)){echo $this->escape($devides[$i]);}else{echo "取得失敗";}?></h2>
-    <?php if(isset($emotions)): ?>
-  <?php for($j=0;$j++;$j<count($emotions)): ?>
-  <label><?php echo $this->escape($emotions['emotion_name']); ?></label>
-  <p><?php echo $this->escape($emotions['mean']); ?></p>
+<?php for($i=0;$i<3;$i++): ?>
+  <article>
+    <p class="devides"><?php if(isset($devides)){echo $this->escape($devides[$i]['name']);}else{echo "取得失敗";}?></p>
+    <?php if(isset($emotion_of_devides)): ?>
+  <?php for($j=0;$j<count($emotion_of_devides[$i]);$j++): ?>
+  <label><?php echo ($j+1).'.'.$this->escape($emotion_of_devides[$i][$j]['name']); ?></label>
+  <p><?php echo $this->escape($emotion_of_devides[$i][$j]['mean']); ?></p>
 <?php endfor; ?>
 <?php endif; ?>
-</section>
+</article>
 <?php endfor; ?>
 </article>
-<a href="<?php echo $base_url; ?>/">トップページへ戻る</a>
+<p class="book">参考:「感情」の解剖図鑑 仕事もプライベートも充実させる、心の操り方/苫米地秀人</p>
+<p><a href="<?php echo $this->escape($base_url); ?>/">トップページへ戻る</a></p>
+</div>
---
+++ views/service/index.php
@@ -0,0 +1,11 @@
+<?php $this->setTitle('title','使い方');?>
+<div>
+  <div class="topImage">
+  <img src="<?php echo $this->escape($stage_url);?>images/bookImage3.jpg">
+  </div>
+<article>
+<p>使い方の説明</p>
+
+</article>
+<p><a href="<?php echo $this->escape($base_url); ?>/">トップページへ戻る</a></p>
+</div>
