b0a5838f484cce40c3a865e63e081fff0fe9f628
tohru ito <itohru550@gmail.com>
2019-09-11 18:07:58 +0900

    日記の閲覧ページ、編集ページの見た目に関わるファイル

--- views/diary/edit.php
+++ views/diary/edit.php
@@ -1,54 +1,68 @@
-<?php$this->setLayout('title','日記編集ページ') ?>
+<?php $this->setTitle('title','日記編集ページ') ?>
 <div>
-  <form action="<?php echo $base_url.'/diary/register'; ?>" method="post">
-  <article>
-    <!-- (年月日曜日を表示する箇所) -->
-    <h2><?php if(isset($date)){echo $this->escape(date('Y年 m/d (D)',$date));}else{echo '取得失敗';} ?></h2>
-    <input name="date" type="hidden" value="<?php if(isset($date)){echo $this->escape(date('Y-m-d',$date));}else{echo '取得失敗';} ?>">
-      <button type= "submit" value="日記を保存">
+  <div class="topImage">
+  <img src="<?php echo $this->escape($stage_url); ?>images/noteImage3.jpg">
+  </div>
+  <form enctype="multipart/form-data" action="<?php echo $base_url ?>/diary/register/<?php echo $this->escape($date); ?>" method="post">
+    <article>
+      <!-- (年月日曜日を表示する箇所) -->
+      <h2><?php if(isset($date)){echo $this->escape(date('Y年 m/d (D)',strtotime($date)));}else{echo '取得失敗';} ?></h2>
+      <input name="date" type="hidden" value="<?php if(isset($date)){echo $this->escape(date('Y-m-d',strtotime($date)));}else{echo '取得失敗';} ?>">
     </article>
-    <?php if(isset($errors)){$this->render('errors',array('errors'=>$errors));}?>
-      <article>
-        <?php for($i=0;$i++;$i<count($feeling)):?>
-          <select name="feeling">
-        <option value="<?php echo $i; ?>"><?php echo $this->escape($feeling[$i]) ?></option>
-      </select>
-      <?php endfor;?>
-        <!-- 入力箇所の部分*5  -->
-        <?php for($i=0;$i++;$i<5):?>
-          <section>
-            <h3>No.<?php echo ($i+1); ?></h3>
-
+    <?php if(count($errors) !== 0){echo $this->render('errors',array('errors'=>$errors));}?>
+    <article>
+      <p>今日の調子は
+        <select name="feeling">
+          <?php for($i=0;$i<count($kinds['feelings']);$i++):?>
+            <option value="<?php echo $i; ?>" <?php if(isset($diary['feeling_id'])&&($kinds['feelings'][$i]['id']==$diary['feeling_id'])){echo 'selected';}  ?>>
+              <?php echo $this->escape($kinds['feelings'][$i]['name']) ?></option>
+          <?php endfor;?>
+        </select>
+        １日！
+      </p>
+      <!-- 入力箇所の部分*5  -->
+      <?php for($i=0;$i<5;$i++):?>
+        <article>
+          <p>
             <label>状況</label>
-            <select name="situation_.<?php echo $i ?>">
-              <?php if(isset($kinds_situations) && isset($situations)): ?>
-                <?php for($j=0;$j++;$j<count($kinds_situations)):?>
-                  <option value="<?php echo $j?>"><?php  echo $this->escape($kinds_situations[$j]);?></option>
+            <select name="situation_<?php echo $i ?>">
+              <?php if(isset($kinds['situations'])): ?>
+                <?php for($j=0;$j<count($kinds['situations']);$j++):?>
+                  <option value="<?php echo $j?>" <?php if(isset($date_contents[$i]['situation_id'])&&($kinds['situations'][$j]['id']==$date_contents[$i]['situation_id'])){echo 'selected';}?>>
+                    <?php  echo $this->escape($kinds['situations'][$j]['name']);?></option>
                 <?php endfor;?>
               <?php endif;?>
             </select>
 
             <label>感情</label>
-            <select name="emotions_.<?php echo $i ?>">
-              <?php if(isset($kinds_emotions) && isset($emotions)): ?>
-                <?php for($k=0;$k++;$k<count($kinds_emotions)):?>
-                  <option value="<?php echo $k;?>"><?php echo $this->escape($kinds_emotions[$k]);?></option>
+            <select name="emotion_<?php echo $i ?>">
+              <?php if(isset($kinds['emotions'])): ?>
+                <?php for($k=0;$k<count($kinds['emotions']);$k++):?>
+                  <option value="<?php echo $k;?>" <?php if(isset($date_contents[$i]['emotion_id'])&&($kinds['emotions'][$k]['id']==$date_contents[$i]['emotion_id'])){echo 'selected';}else{echo '取得できない';}?>>
+                    <?php echo $this->escape($kinds['emotions'][$k]['name']);?></option>
                 <?php endfor;?>
               <?php endif; ?>
             </select>
+          </p>
 
-            <label>内容</label>
-            <textarea name = "content_.<?php echo $i ?>" value="<?php if(isset($contents[$i])) {echo $this->escape($content[$i]);}else{echo '';} ?>"></textarea>
-          </section>
-        <?php endfor;?>
+          <p><label>内容</label></p>
+          <textarea name = "content_<?php echo $i ?>"  rows="4" cols="40" value=""><?php if(isset($date_contents[$i]['content'])) {echo $this->escape($date_contents[$i]['content']);}else{echo '';} ?></textarea>
+        </article>
+      <?php endfor;?>
 
-      </article>
+    </article>
 
-      <article>
-        <!-- (写真を選択する箇所) -->
-      </article>
-      <input type="hidden" name="csrf_token" value="<?php if(isset($token)){echo $this->escape($token);}?>">
-      <button type="submit">保存</button>
-    </form>
-    <a href="<?php echo $this->escape($base_url); ?>/diary/index/<?php if(isset($date)){echo $this->escape($date);} ?>">閲覧ページへ戻る</a>
-        </div>
+    <article>
+      <!-- (写真を選択する箇所) -->
+      <p>今日の一枚<input name="uploadfile" type="file" value="<?php if(isset($diary['picture'])){echo $this->escape($diary['picture']);}?>"></p>
+    </article>
+    <article>
+      <?php if($diary_count==='1'): ?>
+      <p><input type="checkbox" name="delete" value="1">保存されている日記をまっさらにする</p>
+    <?php endif; ?>
+    </article>
+    <input type="hidden" name="csrf_token" value="<?php if(isset($token)){echo $this->escape($token);}?>">
+    <button type="submit">日記を保存</button>
+  </form>
+  <p><a href="<?php echo $this->escape($base_url); ?>/diary/index/<?php if(isset($date)){echo $this->escape($date);} ?>">閲覧ページへ戻る</a></p>
+</div>
--- views/diary/index.php
+++ views/diary/index.php
@@ -1,58 +1,85 @@
-<?php$this->setLayout('title','日記閲覧ページ') ?>
+<?php $this->setTitle('title','日記閲覧ページ') ?>
 <div>
+  <div class="topImage">
+  <img src="<?php echo $this->escape($stage_url); ?>images/noteImage9.jpg">
+  </div>
   <article>
-  <!-- 年月日と曜日を表示する箇所 -->
-  <?php if(isset($errors)){$this->render('errors',array('errors'=>$errors));}?>
-    <form action="<?php echo $base_url.'/diary/edit'; ?>" method="post">
+    <!-- 年月日と曜日を表示する箇所 -->
+    <?php if(isset($errors)){echo $this->render('errors',array('errors'=>$errors));}?>
+    <form action="<?php echo $base_url.'/diary/index'; ?>" method="post">
       <select name="year">
-        <?php for($i=0;$i--;$i>-$select_year['period']):?>
+        <?php for($i=0;$i>-($select_year['period']+1);$i--): ?>
           <option value="<?php echo $this->escape($select_year['last']+$i); ?>"
             <?php if(isset($select_year['last'])&&isset($date)&&(date("Y",strtotime($date))===(date("Y",strtotime($select_year['last']+$i))))){echo 'selected';} ?>>
             <?php echo $this->escape($select_year['last']-$i); ?></option>
-        <?php endfor;?>
-      </select>
-      年
-      <select name="month">
-        <?php for($i=0;$i++;$i<12):?>
-          <option value="<?php echo $this->escape($i+1);?>"
-              <?php if(isset($date)&&(date("m",strtotime($date))===(date("m",strtotime($i+1))))){echo 'selected';} ?>>
-            <?php $this->escape($i+1);?></option>
-        <?php endfor;?>
-      </select>
-      月
-      <select name="day">
-        <?php for($i=0;$i++;$i<31):?>
-          <option value="<?php echo $this->escape($i+1);?>"
-            <?php if(isset($date)&&(date("d",strtotime($date))===(date("d",strtotime($i+1))))){echo 'selected';} ?>>
-            <?php echo $this->escape($i+1);?></option>
-        <?php endfor;?>
-      </select>
-      日の日記ページを<button type="submit">開く</button>
-    </form>
-  <h2><?php if(isset($date)){echo $this->escape(date('Y年 m/d (D)',$date));}else{echo '取得失敗';} ?></h2>
-</article>
-<article>
-  <p>この日の総評は<?php if(isset($diary['feeling'])){echo $this->escape($diary['feeling']);}else{echo '取得失敗';}  ?></p>
-</article>
+          <?php endfor;?>
+        </select>
+        年
+        <select name="month">
+          <?php for($i=0;$i<12;$i++):?>
+            <option value="<?php echo $this->escape($i+1);?>"
+              <?php if(isset($date)&&(($i+1)==date("n",strtotime($date)))){echo 'selected';} ?>>
+              <?php echo  $this->escape($i+1);?></option>
+            <?php endfor;?>
+          </select>
+          月
+          <select name="day">
+            <?php for($i=0;$i<31;$i++):?>
+              <option value="<?php echo $this->escape($i+1);?>"
+                <?php if(isset($date)&&(($i+1)==date("j",strtotime($date)))){echo 'selected';} ?>>
+                <?php echo $this->escape($i+1);?></option>
+              <?php endfor;?>
+            </select>
+            日のページを<button type="submit">開く</button>
+          </form>
+          <h2><?php if(isset($date)){echo $this->escape(date('Y年 n/j (D)',strtotime($date)));}else{echo '取得失敗';} ?></h2>
+        </article>
 
-<article>
-<!-- 取得した内容を表示する箇所 -->
-<?php if (isset($date_contents)):?>
-<?php for($i = 0;$i++;$i<count($date_contents)): ?>
-<section>
-  <h3>No.<?php echo $i+1 ?></h3>
-<p><?php echo 'シチュエーション:'.($this->escape($situations[$i])) ?></p>
-<p><?php echo '気持ち:'.($this->escape($emotions[$i])) ?></p>
-<p><?php echo $this->escape($contents[$i]); ?></p>
-</section>
-<?php endfor; ?>
-<?php endif; ?>
-</article>
+        <?php if($diary===false): ?>
+          <!-- 日記がそもそもない時 -->
+          <article>
+            <p>日記は書かれていません。真っさらです。</p>
+            <form action="<?php echo $this->escape($base_url); ?>/diary/edit/<?php echo $this->escape($date); ?>" method="post">
+              <button type="submit">日記を書く</button>
+            </form>
+            <div class="diaryImage">
+            <img src="<?php echo $this->escape($stage_url); ?>images/noteImage6.jpg">
+            </div>
 
-<article>
-  <!-- 写真を表示する箇所 -->
-  <img>
-</article>
+          </article>
+        <?php else: ?>
+          <!-- 日記がある時 -->
+          <article>
+            <form action="<?php echo $this->escape($base_url); ?>/diary/edit/<?php echo $this->escape($date); ?>" method="post">
+              <button type="submit">日記を書く</button>
+            </form>
+            <p>今日の調子は<?php if(isset($diary['feeling'])){echo '【'.$this->escape($diary['feeling']).'】日。';}else{echo '取得失敗';}  ?></p>
+            <?php if(empty($date_contents)): ?>
+              <!-- 日記はあるが内容がない時 -->
+              <p>出来事は書かれていません。</p>
+            <?php else: ?>
+              <!-- 日記も内容もある時 -->
+              <?php for ($i=0;$i<count($date_contents); $i++): ?>
+                <article class="contents">
+                  <p><?php echo '【'.($this->escape($date_contents[$i]['situation'])).'/' ?>
+                  <?php echo ($this->escape($date_contents[$i]['emotion'])).'】' ?></p>
 
-<a href="<?php echo $this->escape($base_url); ?>/">トップページへ戻る</a>
-</div>
+                  <p class="content"><?php echo $this->escape($date_contents[$i]['content']); ?></p>
+                </article>
+
+              <?php endfor; ?>
+            <?php endif; ?>
+            <article  class="today_image">
+              <!-- 写真を表示する箇所 -->
+              <?php if($diary['picture']===''):?>
+                <p>この日の写真は選択されていません</p>
+              <?php else: ?>
+                <img src="<?php echo $this->escape($stage_url.'../images/'.$diary['picture']);  ?> ">
+              <?php endif; ?>
+
+            </article>
+          <?php endif; ?>
+        </article>
+
+        <p><a href="<?php echo $this->escape($base_url); ?>/">トップページへ戻る</a></p>
+      </div>
