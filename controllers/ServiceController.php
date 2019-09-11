16b0eb98c4c76b8ec02c4026b3426e38b7770fdd
tohru ito <itohru550@gmail.com>
2019-09-11 13:33:05 +0900

    処理の修正。

--- controllers/ServiceController.php
+++ controllers/ServiceController.php
@@ -1,33 +1,37 @@
 <?php
 class ServiceController extends Controller
 {
-  protected $controller_actions = array(
-    'index',
-    'emotions'
-  );
-
-  public function indexAction($parameters) {
+  protected $controller_actions = array();
+  //利用方法や操作説明
+  public function indexAction($parameters)
+  {
     $user = $this->session->get('user');
     $parameters = array_merge($parameters, array('user'=>$user));
-    //利用方法や操作説明
     return $this->render($parameters);
   }
 
-  public function emotionsAction($parameters) {
+  public function emotionsAction($parameters)
+  {
     //各感情の解説および本の紹介
     $user = $this->session->get('user');
-
-    //感情とその一言を取得
     $diary_repository = $this->db_manager->getRepository('diary');
-
-    $devides = $diary_repository-> getKindsDevides();
-
-    for($i=0;$i++;$i<41) {
-      $emotions[$i] = $diary_repository->getKindsEmotions($i);
+    $kinds=$this->getKinds();
+    array_shift($kinds['emotions']);
+    $kinds['emotions'] = array_values($kinds['emotions']);
+    for ($i=0;$i<count($kinds['emotions']);$i++) {
+      if($kinds['emotions'][$i]['devide_id'] === '0'){
+        $emotions_of_positive[] = $kinds['emotions'][$i];
+      } else if($kinds['emotions'][$i]['devide_id'] === '1'){
+        $emotions_of_negative[] = $kinds['emotions'][$i];
+      } else if($kinds['emotions'][$i]['devide_id'] === '2'){
+        $emotions_of_other[] = $kinds['emotions'][$i];
+      }
     }
-
-    //各感情の説明および分類を取得
-    $parameters = array_merge($parameters, array('user'=>$user,'emotions'=>$emotions));
-    $this->render($parameters);
+    $emotions_array=array($emotions_of_positive,$emotions_of_negative,$emotions_of_other);
+    for ($i = 0;$i <count($emotions_array);$i++) {
+      $emotion_of_devides[]=$emotions_array[$i];
+    }
+    $parameters = array_merge($parameters, array('user'=>$user,'devides'=>$kinds['devides'],'emotion_of_devides'=>$emotion_of_devides));
+    return  $this->render($parameters);
   }
 }
