73aa4f54874ad9481b35e556cb355451a1fb8ae9
tohru ito <itohru550@gmail.com>
2019-09-16 10:47:33 +0900

    関数を定義して各emotionsSelect毎に実行するように変更

--- web/js/balloon.js
+++ web/js/balloon.js
@@ -1,93 +1,40 @@
 document.addEventListener("DOMContentLoaded", function(){
   //感情を選択するselectを取得
   let emotionsSelect = document.getElementsByClassName('emotionsSelect');
-  /////以下が1セット//////////
-  emotionsSelect[0].addEventListener("change", function(){
+  let balloon = function(i){
     if(document.getElementById('emotions-tooltips') != null){
       let tooltip = document.getElementById('emotions-tooltips');
       tooltip.parentNode.removeChild(tooltip);
     }
-    console.log('change発火');
+    let select = document.getElementsByClassName('emotionsSelect');
+    let obj = select[i];
     let element = document.createElement("div");
     let contentSelectArea = document.getElementsByClassName('contentSelectArea');
-    element.innerHTML = this[this.value].getAttribute('data-text');
+    element.innerHTML = obj[obj.value].getAttribute('data-text');
     element.id = "emotions-tooltips";
     console.log(element);
-    contentSelectArea[0].insertAdjacentElement( "afterend", element );
-  }, false);
-  /////以上が1セット//////////
-
-  /////以下がセット//////////
-  emotionsSelect[1].addEventListener("change", function(){
-    if(document.getElementById('emotions-tooltips') != null){
-      let tooltip = document.getElementById('emotions-tooltips');
-      tooltip.parentNode.removeChild(tooltip);
-    }
-    console.log('change発火');
-    let element = document.createElement("div");
-    let contentSelectArea = document.getElementsByClassName('contentSelectArea');
-    element.innerHTML = this[this.value].getAttribute('data-text');
-    element.id = "emotions-tooltips";
-    console.log(element);
-    contentSelectArea[1].insertAdjacentElement( "afterend", element );
-  }, false);
-  /////以上がセット//////////
-
-  /////以下がセット//////////
-  emotionsSelect[2].addEventListener("change", function(){
-    if(document.getElementById('emotions-tooltips') != null){
-      let tooltip = document.getElementById('emotions-tooltips');
-      tooltip.parentNode.removeChild(tooltip);
-    }
-    console.log('change発火');
-    let element = document.createElement("div");
-    let contentSelectArea = document.getElementsByClassName('contentSelectArea');
-    element.innerHTML = this[this.value].getAttribute('data-text');
-    element.id = "emotions-tooltips";
-    console.log(element);
-    contentSelectArea[2].insertAdjacentElement( "afterend", element );
-  }, false);
-  /////以上がセット//////////
-
-  /////以下がセット//////////
-  emotionsSelect[3].addEventListener("change", function(){
-    if(document.getElementById('emotions-tooltips') != null){
-      let tooltip = document.getElementById('emotions-tooltips');
-      tooltip.parentNode.removeChild(tooltip);
-    }
-    console.log('change発火');
-    let element = document.createElement("div");
-    let contentSelectArea = document.getElementsByClassName('contentSelectArea');
-    element.innerHTML = this[this.value].getAttribute('data-text');
-    element.id = "emotions-tooltips";
-    console.log(element);
-    contentSelectArea[3].insertAdjacentElement( "afterend", element );
-  }, false);
-  /////以上がセット//////////
-
-  /////以下がセット//////////
-  emotionsSelect[4].addEventListener("change", function(){
-    if(document.getElementById('emotions-tooltips') != null){
-      let tooltip = document.getElementById('emotions-tooltips');
-      tooltip.parentNode.removeChild(tooltip);
-    }
-    console.log('change発火');
-    let element = document.createElement("div");
-    let contentSelectArea = document.getElementsByClassName('contentSelectArea');
-    element.innerHTML = this[this.value].getAttribute('data-text');
-    element.id = "emotions-tooltips";
-    console.log(element);
-    contentSelectArea[4].insertAdjacentElement( "afterend", element );
-  }, false);
-  /////以上がセット//////////
-
+    contentSelectArea[i].insertAdjacentElement( "afterend", element);
+  }
+  emotionsSelect[0].addEventListener("change",function(){
+    balloon(0);
+  },false);
+  emotionsSelect[1].addEventListener("change",function(){
+    balloon(1);
+  },false);
+  emotionsSelect[2].addEventListener("change",function(){
+    balloon(2);
+  },false);
+  emotionsSelect[3].addEventListener("change",function(){
+    balloon(3);
+  },false);
+  emotionsSelect[4].addEventListener("change",function(){
+    balloon(4);
+  },false);
   let body = document.body;
-
   body.addEventListener("click", function(){
     if(document.getElementById('emotions-tooltips') != null){
       let tooltip = document.getElementById('emotions-tooltips');
       tooltip.parentNode.removeChild(tooltip);
     }
   },false);
-
 },false);
